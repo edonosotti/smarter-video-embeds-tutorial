1c6c9f4a41842febe3f25e6d067f46e47d8d9def
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2020-05-20 22:48:19 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Smartify Your Video Embeds - Tutorial
+
+This tutorial shows how to:
+
+ * embed externally hosted videos (on YouTube and Vimeo) into `HTML` pages
+ * embed self-hosted hosted videos into `HTML` pages
+ * make the embeds responsive
+ * use custom cover images on all kinds of videos
+ * set video options
+ * use `JavaScript` to control videos
+
+## Code
+
+See:
+
+ * [demo.html](www/demo.html)
+ * [demo.css](www/demo.css)
+ * [demo.js](www/demo.js)
+
+and check the comments in the files.
+
+## Reference documentation
+
+See:
+
+ - https://www.w3schools.com/tags/tag_video.asp
+ - https://www.w3schools.com/tags/ref_av_dom.asp
+ - https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+ - https://developers.google.com/web/updates/2016/07/autoplay
+ - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+ - https://stackoverflow.com/questions/50287175/html5-video-autoplay-feature-not-working-without-muted/50287806
+ - https://stackoverflow.com/questions/46745684/muted-autoplay-videos-stop-playing-in-safari-11-0
+
+## Kudos
+
+This demo uses code and assets from:
+
+ - [Bootstrap](https://getbootstrap.com)
+ - [FitVids.js](http://fitvidsjs.com)
+ - [jQuery](https://jquery.com)
+ - [FontAwesome](https://fontawesome.com)
+ - [Google Fonts](https://fonts.google.com)
+
+External videos are randomly picked from [YouTube](https://www.youtube.com) and [Vimeo](https://vimeo.com) and were publicly available at the time of writing. Cover images are picked from [Unsplash](https://unsplash.com) and served by [Lorem Picsum](https://picsum.photos). Some videos are picked from [Pexels](https://www.pexels.com).
+
+Code has been built on [JSFiddle](https://jsfiddle.net), hosting for the demo files is provided by [GitHub Pages](https://pages.github.com).
---
+++ www/demo.js
@@ -0,0 +1,94 @@
+function getVideoEmbeds() {
+  /* Selectors were taken from FitVids.js and edited:
+   * https://github.com/davatron5000/FitVids.js/blob/f72b623e466bfe596b5800458d6043675ffb091b/jquery.fitvids.js
+   * It is worth checking that code because it has
+   * some possibly handy additional features, not
+   * ported here to keep this tutorial simple. */
+  var selectors = [
+    'iframe[src*="player.vimeo.com"]',
+    'iframe[src*="youtube.com"]',
+    'iframe[src*="youtube-nocookie.com"]',
+    'iframe[src*="kickstarter.com"][src*="video.html"]',
+    'video'
+  ];
+
+  var videos = $('body').find(selectors.join(','));
+  // Again, kudos to the FitVids.js team!
+  return videos.not('.do-not-resize');
+}
+
+function resizeVideos(elements) {
+  $(elements).each(function() {
+    var width = $(this).width();
+    var height = parseInt((width * 9) / 16);
+    $(this).height(height);
+  });
+}
+
+function setCustomCovers(elements) {
+  $(elements).each(function() {
+    var customCover = $(this).data("custom-cover");
+    if (customCover) {
+      var coverElement = $('<a href="#" class="custom-video-cover"></a>');
+      var label = $(this).data("custom-cover-label");
+      if (label) {
+        coverElement.html(label);
+      }
+      coverElement.css('background-image', 'url(' + customCover + ')');
+      coverElement.width($(this).width());
+      coverElement.height($(this).height());
+      coverElement.data('video-element', $(this));
+      $(this).replaceWith(coverElement);
+    }
+  });
+
+  setCustomCoverEventListeners();
+}
+
+function setCustomCoverEventListeners() {
+  $('.custom-video-cover').click(function(e) {
+    e.preventDefault();
+    $(this).replaceWith($(this).data('video-element'));
+  });
+}
+
+function setCustomVideoControls() {
+  /* Look for a <video> element wrapped into the same
+   * container as the elements with the '.btn-play' and
+   * '.btn-pause' classes and call the video API.
+   * This approach keeps the HTML code easy and clean,
+   * but it requires the <video> elements and their
+   * controls to be wrapped in the same container. */
+  $('.btn-play').click(function(e) {
+    e.preventDefault();
+    $(this).parent().children('video')[0].play();
+  });
+  $('.btn-pause').click(function(e) {
+    e.preventDefault();
+    $(this).parent().children('video')[0].pause();
+  });
+}
+
+function setCustomSelectorVideoControls() {
+  /* Look for a <video> element using the selector
+   * provided with the data-video-selector="" attribute
+   * of the button tag. This approach is more flexible,
+   * it allows free placement of the <video> elements
+   * and their control elements. */
+  $('.btn-play-selector').click(function(e) {
+    e.preventDefault();
+    $($(this).data('video-selector'))[0].play();
+  });
+  $('.btn-pause-selector').click(function(e) {
+    e.preventDefault();
+    $($(this).data('video-selector'))[0].pause();
+  });
+}
+
+$(function() {
+  var videos = getVideoEmbeds();
+  resizeVideos(videos);
+  setCustomCovers(videos);
+  setCustomVideoControls();
+  setCustomSelectorVideoControls();
+});
